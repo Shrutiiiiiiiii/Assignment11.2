d31d5a481cc27895ab04415135173981a1d63864
Shruti Dhoot <shrutidhoot2001@gmail.com>
2024-12-03 11:34:10 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'books', 
+    component: BooksComponent 
+    },
+
+    { path: 'technologies',
+     component: TechnologiesComponent 
+    },
+
+];
